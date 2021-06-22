9759e432af3c11848e49441bb1b7a15ec697f382
mtardif45 <61833712+mtardif45@users.noreply.github.com>
2021-06-22 17:20:16 +0200

    password email pseudo validator

---
+++ backend/middleware/auth-validator.js
@@ -0,0 +1,39 @@
+const emailValidator = require("email-validator");
+const passwordValidator = require("password-validator");
+
+exports.validInput = (req, res, next) => {
+    const passwordSchema = new passwordValidator();
+    passwordSchema
+        .is().min(8)
+        .is().max(30)
+        .has().uppercase()
+        .has().lowercase()
+        .has().not().symbols()
+        .has().not().spaces();
+
+    if (
+        !emailValidator.validate(req.body.email) ||
+        !passwordSchema.validate(req.body.password)
+    ) {
+        return res.status(400).send({
+            error: "your password must contain at least 8 characters with lower and uppercase"
+        });
+    } else if (
+        emailValidator.validate(req.body.email) ||
+        passwordSchema.validate(req.body.password)
+    ) {
+        next();
+    }
+};
+
+exports.validPseudo = (req, res, next) => {
+    const regex = /^[a-zA-Z0-9_]{3,20}$/; // entre 3 et 20 caractères, majuscules minuscules espaces
+    const pseudo = req.body.pseudo;
+    if (regex.test(pseudo) === true) {
+        next();
+    } else {
+        return res.status(400).send({
+            error: "your pseudo must contain between 3 and 20 characters"
+        });
+    }
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
+const authValidator = require("../middleware/auth-validator");
 const userCtrl = require("../controllers/user");
 
-router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.login);
-router.get("/accounts/:id", userCtrl.getAccount);
+router.post("/signup", authValidator.validPseudo, authValidator.validInput, userCtrl.signup);
+router.post("/login", authValidator.validInput, userCtrl.login);
+router.get("/accounts/:id", auth, userCtrl.getAccount);
 router.get("/accounts", auth, userCtrl.getAllUsers);
 
-router.put("/accounts/:id", multer, userCtrl.updateAccount);
+router.put("/accounts/:id", auth, multer, userCtrl.updateAccount);
 
 router.delete("/accounts/:id", auth, userCtrl.deleteAccount);
 
