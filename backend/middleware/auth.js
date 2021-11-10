91bc39f28fef254b76f6b15f7dcf13a289efbf50
mtardif45 <61833712+mtardif45@users.noreply.github.com>
2021-11-10 10:29:23 +0100

    .env complet login auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 module.exports = async (req, res, next) => {
     //gestion des erreurs avec bloc try and catch
     try {
         const token = req.headers.authorization; // on récupère le token de la requête entrante dans le header d'autorisation
-        const decodedToken = await jwt.verify(token, 'RANDOM_TOKEN'); // on le vérifie
+        const decodedToken = await jwt.verify(token, process.env.SECRET); // on le vérifie
         console.log(decodedToken);
         const userId = decodedToken.userId;// on récupère le user id du token
         if (req.body.userId && req.body.userId !== userId) { // on compare le userid de la requête à celui du token
